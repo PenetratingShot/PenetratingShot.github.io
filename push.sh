51b5dc225bea9fda1ed39675ce700b4a3a42f34d
Shreyas Lad <slad0716@gmail.com>
2019-04-12 20:17:35 +0000

    hmm

--- push.sh
+++ push.sh
@@ -1,17 +1,17 @@
 add() {
     git add .
-    echo "Enter your commit message: "
+    printf "Enter your commit message: "
     read message
     git commit -m "$message"
     
     if [[ -z "$message" ]]; then
-        echo "\e[31mError: you must enter a commit message\e[39m\n"
+        printf "\e[31mError: you must enter a commit message\e[39m\n"
         add
     fi
 }
 
 git add .
-echo "Enter your commit message: "
+printf "Enter your commit message: "
 read message
 git commit -m "$message"
 
@@ -20,30 +20,30 @@ if [[ -z "$message" ]]; then
     add
 fi
 
-echo "Source: "
+printf "Source: "
 read source
 
 #user doesn't specify remote
 if [[ -z "$source" ]]; then
     #promt user for branch
-    echo "Branch: "
+    printf "Branch: "
     read branch
     #user doesn't specify branch
     if [[ -z "$branch" ]]; then
         #pushing to default settings
         git push origin master
     else
-        echo "git push origin $branch"
+        printf "git push origin $branch"
     fi
 #source
 else
-   echo "Branch: "
+   printf "Branch: "
    read branch2
    #user dosen't input branch but has inputted a remote
    if [[ -z "$branch2" ]]; then
-        echo "git push $source master"
+        printf "git push $source master"
    else
-        echo "git push $source $branch2"
+        printf "git push $source $branch2"
    fi
 fi
  
